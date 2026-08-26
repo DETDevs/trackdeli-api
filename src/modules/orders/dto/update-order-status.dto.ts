424fe89f6ff442c1031131f14c472d288a2e9f79
edwin08torres <at2899743@gmail.com>
2026-08-25 20:56:16 -0600

    fix order status

--- src/modules/orders/dto/update-order-status.dto.ts
+++ src/modules/orders/dto/update-order-status.dto.ts
@@ -2,7 +2,7 @@ import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { OrderStatus } from '@prisma/client';
 
 export class UpdateOrderStatusDto {
-  @IsEnum(OrderStatus)
+  @IsEnum(OrderStatus, { message: 'status debe ser un valor válido de OrderStatus' })
   @IsNotEmpty()
   status: OrderStatus;
 
